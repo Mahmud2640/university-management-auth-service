97f63faf312544ee86ac41ec2cb4b2744cc5aff2
Mahmud2640 <mdmahmud484@gmail.com>
2023-08-06 12:26:23 +0600

    corse

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-const express = require("express");
+import express, { Application, Request, Response } from "express";
 const app: Application = express();
+
+import cors from "cors";
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working Succcesfully!");
 });
 
 export default app;
