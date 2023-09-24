21e7c265bf8f87ec9fda7c37b2c4a4b01ef873d1
Mahmud2640 <mdmahmud484@gmail.com>
2023-09-25 01:03:01 +0600

    loggertime

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,23 +1,44 @@
-import winston = require('winston')
+import { createLogger, format, transports } from 'winston'
 import path from 'path'
 
-const logger = winston.createLogger({
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+// Custom Log Formet
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'Testing' }),
+    timestamp(),
+    myFormat,
+    prettyPrint(),
+  ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
       level: 'info',
     }),
   ],
 })
-const errorLogger = winston.createLogger({
+const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'Testing' }),
+    timestamp(),
+    myFormat,
+    prettyPrint(),
+  ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error',
     }),
